7359374ce544700af6ec1439fa1092475443f5d1
JoelBeau <joelbravo@protonmail.com>
2024-09-06 13:32:22 -0500

    Updated setup script to add bin to path variable

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-#Install dependencies
-sudo ../scripts/install_depend.sh
+# #Install dependencies
+# sudo ../scripts/install_depend.sh
 
-#Create the database
-sudo ../scripts/create_thermo_database.sh
+# #Create the database
+# sudo ../scripts/create_thermo_database.sh
+
+#Add the bin directory to the PATH
+echo "export PATH=\$PATH:$(pwd)/bin" >> ~/.bashrc
+
+# # Apply the changes to the current session
+source ~/.bashrc
