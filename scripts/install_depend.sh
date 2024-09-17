8cd8f9d07e2377e2ec41eb145a8bbc03763315c0
JoelBeau <joelbravo@protonmail.com>
2024-09-17 06:54:30 -0500

    Updated install_depend script

--- scripts/install_depend.sh
+++ scripts/install_depend.sh
@@ -14,8 +14,6 @@ sudo apt-get install pip3 -y
 sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install pkg-config -y
 
-USER="$(whoami)"
-
 sudo service mysql start
 
 #Create the database
@@ -23,17 +21,11 @@ sudo mysql <<EOF
 CREATE DATABASE IF NOT EXISTS thermo;
 EOF
 
-PASSWORD=""
-
-read -p "Enter the SQL username: " USER
-
-read -sp "Enter the SQL password: " PASSWORD
-
 echo "Creating user mysql user $USER..."
 
 sudo mysql <<EOF
-CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD';
-GRANT ALL PRIVILEGES ON thermo.* TO '$USER'@'localhost';
+CREATE USER 'thermo_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON thermo.* TO 'thermo_user'@'localhost';
 EOF
 
 echo "User $USER created and given access to thermo database"
